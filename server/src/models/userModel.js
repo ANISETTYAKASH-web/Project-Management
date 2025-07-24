b27fc764c7d1cf0ef63df078004d324d1cf9f50e
akash <akashanisetty7@gmail.com>
2025-07-24 09:18:21 +0100

    Defined User Schema

---
+++ server/src/models/userModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const userSchema = mongoose.Schema({
+  user_name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+export default mongoose.model("User", userSchema);
