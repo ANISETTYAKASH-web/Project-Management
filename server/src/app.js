9e72d48818aaadbc54bb62468f45e9cab4a18301
akash <akashanisetty7@gmail.com>
2025-07-24 12:29:03 +0100

    Added few packages

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,8 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import * as cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
+//user defined imports
 const app = express();
 const PORT = process.env.PORT;
 //MiddleWare
