16ee6ee64a52c51804ee330eb18c8932f3d90f17
akash <akashanisetty7@gmail.com>
2025-07-25 15:38:48 +0100

    Included projects Route and made sure that is db is connected

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import projectRouter from "./routes/projectRoutes.js";
+import { connection } from "./config/mongodb.config.js";
 //user defined imports
 const app = express();
 const PORT = process.env.PORT;
@@ -10,10 +12,19 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 //Listening on Port
-
+const dbConnection = async (params) => {
+  try {
+    await connection();
+  } catch (error) {
+    console.log("DB CONNECTION FAILED", error);
+    process.exit(1);
+  }
+};
+dbConnection();
 app.get("/", (req, res) => {
   res.send("Hello World! hey");
 });
-app.listen(PORT, () => {
+app.use("/projects", projectRouter);
+app.listen(PORT, async () => {
   console.log("Listening on port:", PORT);
 });
