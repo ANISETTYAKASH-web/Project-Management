8b1481260832d57b88e429b5c30760b766f2039c
akash <akashanisetty7@gmail.com>
2025-07-25 14:50:56 +0100

    Updated project seeder to generate unique names

--- server/src/seeders/seedProjects.js
+++ server/src/seeders/seedProjects.js
@@ -3,16 +3,16 @@ import { faker } from "@faker-js/faker";
 // CJS
 // const { faker } = require("@faker-js/faker");
 
-function createRandomProjects() {
-  return {
-    name: faker.commerce.product(), // 'Computer'
+const names = faker.helpers.uniqueArray(faker.word.sample, 25);
+let i = 0;
+
+const projects = faker.helpers.multiple(
+  () => ({
+    name: names[i++], // will generate 1000 unique email addresses
     description: faker.commerce.productDescription(), // 'Featuring Phosphorus-enhanced technology, our Fish offers unparalleled Modern performance'
     teamName: faker.company.buzzNoun(), // 'paradigms'
-    budget: faker.finance.amount(), // '617.87'
-  };
-}
-
-const projects = faker.helpers.multiple(createRandomProjects, {
-  count: 20,
-});
+    budget: faker.finance.amount(), // '617.87'}), {
+  }),
+  { count: 20 }
+);
 export default projects;
