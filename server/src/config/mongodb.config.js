be0ae098653ee25af0daa75e83033e4dff9aadc7
akash <akashanisetty7@gmail.com>
2025-07-25 15:34:01 +0100

    Updated DB Config to make it a function to call in app.js

--- server/src/config/mongodb.config.js
+++ server/src/config/mongodb.config.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
-export const connection = await mongoose
-  .connect(process.env.MONGO_DB_URL)
-  .then(() => {
-    console.log("DataBase Connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+export const connection = async () => {
+  await mongoose
+    .connect(process.env.MONGO_DB_URL)
+    .then(() => {
+      console.log("DataBase Connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
