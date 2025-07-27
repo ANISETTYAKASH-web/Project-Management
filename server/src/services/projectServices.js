1c9fa60ad1d72c36fe8a193569a7b020b68c823e
akash <akashanisetty7@gmail.com>
2025-07-27 15:39:53 +0100

    Updated according to new Schema

--- server/src/services/projectServices.js
+++ server/src/services/projectServices.js
@@ -1,20 +1,42 @@
 import Projects from "../models/projectModel.js";
-export async function getAllProjects() {
-  return await Projects.find({});
+import userProjects from "../models/usersProject.Model.js";
+export async function getAllProjects(data) {
+  const res = await userProjects.find(data).populate("Project");
+  // console.log(res);
+  return res.map((project) => project.Project);
 }
-export async function getProjectByName(name) {
-  return await Projects.find({ name: name });
+export async function getProjectByName(name, user) {
+  // return await Projects.find({ name: name });
+  const res = await userProjects
+    .find({ User: user.user_id })
+    .populate("Project");
+  // console.log("user projectss", res);
+  const ans = res.find((project) => {
+    // console.log(project.Project);
+    // console.log(name);
+    if (project.Project.name == name) {
+      // console.log("project", project);
+      return project;
+    }
+  });
+  // console.log("ans", ans);
+  return ans;
 }
-export async function authorized(data) {
-  return await Projects.find(data);
-}
+
 //here data is a object
-export async function createProject(data) {
-  return await Projects.create(data);
+export async function createProject(data, user) {
+  const newProject = await Projects.create(data);
+  await userProjects.create({ User: user.user_id, Project: newProject._id });
+  return newProject;
 }
 export async function updateProject(filter, data) {
+  // console.log(data);
   return await Projects.updateOne(filter, data);
 }
-export async function deleteProject(data) {
-  return await Projects.deleteOne(data);
+export async function deleteProject(data, project_id) {
+  // console.log(data);
+  const delet = await Projects.deleteOne(data);
+  // console.log(delet);
+  await userProjects.deleteMany({ Project: project_id });
+  return delet;
 }
