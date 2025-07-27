bbfe7ebba74dfcc3bdc7b0a7ea9b4dbcd8b81cde
akash <akashanisetty7@gmail.com>
2025-07-27 15:37:58 +0100

    Updated middleware to include user in the body and pass into next func

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -6,14 +6,16 @@ const AuthorizeUser = (req, res, next) => {
   if (token == null) {
     return res.json({ message: "Unauthorized Login Again" });
   }
-  verify(token, process.env.JWT_ACCESS_KEY, (err, user_name) => {
+  verify(token, process.env.JWT_ACCESS_KEY, (err, user) => {
     if (err) {
       return res.json({
         message: "Unauthorized, Please Login Again",
         error: err.message,
       });
     }
-    req.user_name = user_name;
+    // req.user_name = user.user_name;
+    // req.user_id = user.user_id;
+    req.user = { user_name: user.user_name, user_id: user.user_id };
     next();
   });
 };
