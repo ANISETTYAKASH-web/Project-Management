7c4721dfbf4e9d759f58cadc50c443246aa46ab3
akash <akashanisetty7@gmail.com>
2025-07-27 15:37:09 +0100

    Added Role option in Schema usefull in collobration

--- server/src/models/usersProject.Model.js
+++ server/src/models/usersProject.Model.js
@@ -2,6 +2,14 @@ import mongoose from "mongoose";
 const userProjects = new mongoose.Schema({
   User: { type: mongoose.Types.ObjectId, ref: "User" },
   Project: { type: mongoose.Types.ObjectId, ref: "Project" },
+  Role: {
+    type: String,
+    enum: {
+      values: ["owner", "member"],
+      message: "Values can only be owner and member",
+    },
+    default: "owner",
+  },
 });
 
 export default mongoose.model("userProjects", userProjects);
