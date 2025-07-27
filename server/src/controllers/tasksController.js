e13fc881b2d8e59e1c80e91716a6055d226699f9
akash <akashanisetty7@gmail.com>
2025-07-27 16:45:03 +0100

    updated TasksController for CRUD

--- server/src/controllers/tasksController.js
+++ server/src/controllers/tasksController.js
@@ -1,3 +1,4 @@
+import { isAuthorized } from "../services/projectServices.js";
 import {
   getAllTask,
   getTaskByName,
@@ -6,8 +7,22 @@ import {
   deleteTask,
 } from "../services/tasksService.js";
 const getAllTasks = async (req, res) => {
+  const { project_id } = req.query;
+  const UserId = req.user.user_id;
+  console.log(UserId);
+  console.log(project_id);
   try {
-    const tasks = await getAllTask();
+    const isMember = await isAuthorized({
+      User: UserId,
+      Project: project_id,
+    });
+    console.log(isMember);
+    if (!isMember) {
+      return res
+        .status(401)
+        .json({ message: "You are not authorized to access this project" });
+    }
+    const tasks = await getAllTask({ User: UserId, Project: project_id });
     if (!tasks) {
       return res
         .status(200)
@@ -23,11 +38,28 @@ const getAllTasks = async (req, res) => {
 
 const getTasksByName = async (req, res) => {
   const { name } = req.body;
+  const { project_id } = req.query;
+
+  const UserId = req.user.user_id;
   if (!name) {
     return res.status(401).json({ message: "Insufficient Details" });
   }
   try {
-    const task = await getTaskByName({ name: name });
+    const isMember = await isAuthorized({
+      User: UserId,
+      Project: project_id,
+    });
+    if (!isMember) {
+      return res
+        .status(401)
+        .json({ message: "You are not authorized to access this project" });
+    }
+
+    const task = await getTaskByName({
+      name: name,
+      User: UserId,
+      Project: project_id,
+    });
     if (!task) {
       return res
         .status(200)
@@ -42,15 +74,17 @@ const getTasksByName = async (req, res) => {
 };
 
 const createTasks = async (req, res) => {
-  const { name, status, user, project } = req.body;
+  const { name, status } = req.body;
+  const { project_id } = req.query;
   if (!name) {
     return res.status(401).json({ message: "Insufficient Details" });
   }
+  const UserId = req.user.user_id;
   try {
-    const task = { name: name };
+    const task = { name: name, User: UserId, Project: project_id };
     if (status) task.status = status;
-    if (user) task.User = user;
-    if (project) task.Project = project;
+    // if (user) task.User = user;
+    // if (project) task.Project = project;
     const newTask = await createTask(task);
     res.status(200).json({ message: "Success", tasks: newTask });
   } catch (error) {
@@ -61,15 +95,32 @@ const createTasks = async (req, res) => {
 };
 
 const updateTasks = async (req, res) => {
-  const { name, status, user, project } = req.body;
-  if (!name || !status || !project) {
+  const { name, status } = req.body;
+  const UserId = req.user.user_id;
+  const { project_id } = req.query;
+  if (!name || !status) {
     return res.status(401).json({ message: "Insufficient Details" });
   }
-  const task = { name: name, status: status };
-  if (user) task.User = user;
-  if (project) task.Project = project;
+
+  const task = { name: name, User: UserId, Project: project_id };
+  if (status) task.status = status;
+  // if (project) task.Project = project;
   try {
-    const check = getTaskByName({ name: name });
+    const isMember = await isAuthorized({
+      User: UserId,
+      Project: project_id,
+    });
+    if (!isMember) {
+      return res
+        .status(401)
+        .json({ message: "You are not authorized to access this project" });
+    }
+
+    const check = getTaskByName({
+      name: name,
+      User: UserId,
+      Project: project_id,
+    });
     if (check.length === 0) {
       return res.status(401).json({ message: "Task Doesn't Exist" });
     }
@@ -83,23 +134,39 @@ const updateTasks = async (req, res) => {
 };
 
 const deleteTasks = async (req, res) => {
-  const { name, status, user, project } = req.body;
-  if (!name || !project) {
+  const { name } = req.body;
+  if (!name) {
     return res.status(401).json({ message: "Insufficient Details" });
   }
+  const { project_id } = req.query;
+  const UserId = req.user.user_id;
   try {
-    const task = { name: name, Project: project };
-    if (user) task.User = user;
-    // if (project) task.Project = project;
-    console.log(task);
-    const check = getTaskByName({ name: name });
+    const isMember = await isAuthorized({
+      User: UserId,
+      Project: project_id,
+    });
+    if (!isMember) {
+      return res
+        .status(401)
+        .json({ message: "You are not authorized to access this project" });
+    }
 
+    const check = getTaskByName({
+      name: name,
+      User: UserId,
+      Project: project_id,
+    });
     if (check.length === 0) {
       return res.status(401).json({ message: "Task Doesn't Exist" });
     }
-    console.log(check);
-    const tasks = await deleteTask(task);
-    res.status(200).json({ message: "Successfully Deleted", task: tasks });
+
+    // console.log(check);
+    const task = { name: name, User: UserId, Project: project_id };
+
+    const deletedTasks = await deleteTask(task);
+    res
+      .status(200)
+      .json({ message: "Successfully Deleted", task: deletedTasks });
   } catch (error) {
     res
       .status(404)
