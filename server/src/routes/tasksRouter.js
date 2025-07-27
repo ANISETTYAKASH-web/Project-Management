2bd7272104f632b81ba2c52bf06ea3ef53a73361
akash <akashanisetty7@gmail.com>
2025-07-27 07:10:16 +0100

    Updated so only authorized(logged in) users can access task routes

--- server/src/routes/tasksRouter.js
+++ server/src/routes/tasksRouter.js
@@ -6,11 +6,12 @@ import {
   getAllTasks,
   getTasksByName,
 } from "../controllers/tasksController.js";
+import AuthorizeUser from "../middleware/auth.middleware.js";
 
 const TaskRouter = express.Router();
-TaskRouter.get("/getAllTasks", getAllTasks);
-TaskRouter.get("/getTask", getTasksByName);
-TaskRouter.post("/create", createTasks);
-TaskRouter.delete("/delete", deleteTasks);
-TaskRouter.put("/update", updateTasks);
+TaskRouter.get("/getAllTasks", AuthorizeUser, getAllTasks);
+TaskRouter.get("/getTask", AuthorizeUser, getTasksByName);
+TaskRouter.post("/create", AuthorizeUser, createTasks);
+TaskRouter.delete("/delete", AuthorizeUser, deleteTasks);
+TaskRouter.put("/update", AuthorizeUser, updateTasks);
 export default TaskRouter;
